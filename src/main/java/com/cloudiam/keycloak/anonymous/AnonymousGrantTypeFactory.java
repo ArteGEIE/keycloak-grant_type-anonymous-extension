13aede7cb8ab10f13e01998cb441f290e5f90879
joanlena <joan-lena.dieye@liksi.fr>
2025-05-13 17:17:10 +0200

    Wip anonymous grant type provider and factory.

--- src/main/java/com/cloudiam/keycloak/anonymous/AnonymousGrantTypeFactory.java
+++ src/main/java/com/cloudiam/keycloak/anonymous/AnonymousGrantTypeFactory.java
@@ -4,32 +4,38 @@ import org.jboss.logging.Logger;
 import org.keycloak.Config;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
-import org.keycloak.protocol.oidc.grants.OAuth2GrantTypeFactory;
-import org.keycloak.protocol.oidc.grants.OAuth2GrantType;
+import org.keycloak.services.resource.RealmResourceProvider;
+import org.keycloak.services.resource.RealmResourceProviderFactory;
 
-public class AnonymousGrantTypeFactory implements OAuth2GrantTypeFactory {
+public class AnonymousGrantTypeFactory implements RealmResourceProviderFactory {
     private static final Logger logger = Logger.getLogger(AnonymousGrantTypeFactory.class);
-    public static final String PROVIDER_ID = "anonymous-grant-type";
+    public static final String PROVIDER_ID = "default";
+
+    @Override
+    public RealmResourceProvider create(KeycloakSession session) {
+        return new AnonymousGrantTypeProvider(session);
+    }
+
+    @Override
+    public void init(Config.Scope config) {
+        logger.info("******** INITIALIZING ANONYMOUS GRANT TYPE PROVIDER ********");
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+
+    }
+
+    @Override
+    public void close() {
+        logger.info("******** CLOSING ANONYMOUS GRANT TYPE PROVIDER ********");
+    }
 
     @Override
     public String getId() {
         return PROVIDER_ID;
     }
 
-    @Override
-    public OAuth2GrantType create(KeycloakSession session) {
-        return null;
-    }
 
-    @Override
-    public void init(Config.Scope config) {
-    }
 
-    @Override
-    public void postInit(KeycloakSessionFactory factory) {
-    }
-
-    @Override
-    public void close() {
-    }
 }
