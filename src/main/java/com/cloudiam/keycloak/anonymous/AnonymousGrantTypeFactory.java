650212cb1c2afe65c4e7d6afd4de60a1f466859a
joanlena <joan-lena.dieye@liksi.fr>
2025-05-14 10:11:10 +0200

    Fix AnonymousGrantType spi, factory and provider interface implementations, and docker container name.

--- src/main/java/com/cloudiam/keycloak/anonymous/AnonymousGrantTypeFactory.java
+++ src/main/java/com/cloudiam/keycloak/anonymous/AnonymousGrantTypeFactory.java
@@ -4,38 +4,39 @@ import org.jboss.logging.Logger;
 import org.keycloak.Config;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
-import org.keycloak.services.resource.RealmResourceProvider;
-import org.keycloak.services.resource.RealmResourceProviderFactory;
+import org.keycloak.protocol.oidc.grants.OAuth2GrantType;
+import org.keycloak.protocol.oidc.grants.OAuth2GrantTypeFactory;
 
-public class AnonymousGrantTypeFactory implements RealmResourceProviderFactory {
+public class AnonymousGrantTypeFactory implements OAuth2GrantTypeFactory {
     private static final Logger logger = Logger.getLogger(AnonymousGrantTypeFactory.class);
-    public static final String PROVIDER_ID = "default";
 
     @Override
-    public RealmResourceProvider create(KeycloakSession session) {
+    public OAuth2GrantType create(KeycloakSession session) {
         return new AnonymousGrantTypeProvider(session);
     }
 
     @Override
     public void init(Config.Scope config) {
-        logger.info("******** INITIALIZING ANONYMOUS GRANT TYPE PROVIDER ********");
+        logger.info("******** INITIALIZING ANONYMOUS GRANT TYPE FACTORY ********");
     }
 
     @Override
     public void postInit(KeycloakSessionFactory factory) {
-
+        logger.info("******** POST INIT ANONYMOUS GRANT TYPE FACTORY ********");
     }
 
     @Override
     public void close() {
-        logger.info("******** CLOSING ANONYMOUS GRANT TYPE PROVIDER ********");
+        logger.info("******** CLOSING ANONYMOUS GRANT TYPE FACTORY ********");
     }
 
     @Override
     public String getId() {
-        return PROVIDER_ID;
+        return AnonymousGrantTypeProvider.GRANT_TYPE;
     }
 
-
-
+    @Override
+    public int order() {
+        return 100;
+    }
 }
--- src/main/java/com/cloudiam/keycloak/anonymous/AnonymousGrantTypeProvider.java
+++ src/main/java/com/cloudiam/keycloak/anonymous/AnonymousGrantTypeProvider.java
@@ -1,60 +1,37 @@
 package com.cloudiam.keycloak.anonymous;
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.core.Response;
 
 import org.jboss.logging.Logger;
+import org.keycloak.events.EventType;
 import org.keycloak.models.KeycloakSession;
-import org.keycloak.services.resource.RealmResourceProvider;
-import org.keycloak.utils.MediaType;
+import org.keycloak.protocol.oidc.grants.OAuth2GrantType;
 
-public class AnonymousGrantTypeProvider implements RealmResourceProvider {
-
+public class AnonymousGrantTypeProvider implements OAuth2GrantType {
     private static final Logger LOGGER = Logger.getLogger(AnonymousGrantTypeProvider.class);
-    public static final String ANONYMOUS_GRANT_TYPE = "anonymous";
+    
+    public static final String GRANT_TYPE = "anonymous";
     private final KeycloakSession session;
-
 
     public AnonymousGrantTypeProvider(KeycloakSession session) {
         this.session = session;
+        LOGGER.info("AnonymousGrantTypeProvider initialized");
     }
 
-    @Override
-    public Object getResource() {
-        return this;
-    }
-
-    @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response token(MultivaluedMap<String, String> formParams) {
-        LOGGER.info("Custom token endpoint invoked");
-        String grantType = formParams.getFirst("grant_type");
-
-        if ("anonymous".equals(grantType)) {
-            return handleAnonymousGrant(formParams);
-        }
-
-        return delegateToDefaultImplementation(formParams);
-    }
-
-    private Response handleAnonymousGrant(MultivaluedMap<String, String> formParams) {
-        LOGGER.info("Handling anonymous grant type");
-        return Response.ok().entity("Anonymous grant type handled").build();
-    }
-
-    private Response delegateToDefaultImplementation(MultivaluedMap<String, String> formParams) {
-        return Response.status(Response.Status.NOT_IMPLEMENTED)
-                .entity("Default grant types not implemented in this example")
-                .build();
-    }
 
     @Override
     public void close() {
         LOGGER.info("******** CLOSING ANONYMOUS GRANT TYPE PROVIDER ********");
         session.close();
+    }
+
+    @Override
+    public EventType getEventType() {
+        return null;
+    }
+
+    @Override
+    public Response process(Context context) {
+        return null;
     }
 }
